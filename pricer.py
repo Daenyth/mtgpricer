7060b7a9fb44d2b3fd322b473584597476836ec0
Daenyth <Daenyth+git@gmail.com>
2011-06-22 17:28:03 -0400

    pricer: floatify card prices

--- pricer.py
+++ pricer.py
@@ -7,7 +7,7 @@ import urllib2
 
 def getprice(cardname):
     cardinfo = getcard(cardname)
-    return min(c['price'] for c in cardinfo)
+    return min(float(c['price']) for c in cardinfo)
 
 def getcard(cardname):
     url = 'http://blacklotusproject.com/json/?cards=' + urllib.quote(cardname)
